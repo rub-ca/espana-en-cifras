5d5344d1ad89ecfdff4613b27692b0ca3fd8464c
rub-ca <ruben.cardin.d@gmail.com>
2025-06-30 01:45:02 +0200

    wip

--- src/components/poblacion/TdFirstCell.jsx
+++ src/components/poblacion/TdFirstCell.jsx
@@ -1,7 +1,7 @@
 // TdFirstCell.jsx
 const TdFirstCell = ({ children, onClick, cursorPointer }) => {
   return (
-    <td className={cursorPointer ? 'mouse-cursor-pointer' : ''} onClick={onClick}>
+    <td className={cursorPointer ? 'mouse-cursor-pointer pob-align-center' : 'pob-align-center'} onClick={onClick}>
       {children}
     </td>
   )
