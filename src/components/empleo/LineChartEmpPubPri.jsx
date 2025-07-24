d6160fffb778ce589ea20a1e70ca719bfa4d1e8a
rub-ca <ruben.cardin.d@gmail.com>
2025-07-24 17:23:42 +0200

    wip

--- src/components/empleo/LineChartEmpPubPri.jsx
+++ src/components/empleo/LineChartEmpPubPri.jsx
@@ -3,13 +3,15 @@ import React, { lazy, Suspense } from "react"
 const ReactECharts = lazy(() => import('echarts-for-react'))
 
 const LineChartEmpPubPri = ({ numSeries, seriesData, seriesNames }) => {
-
     const numYears = seriesData[0].length
 
     const yearsLegend = []
     for (let i = 0; i < numYears; i++) {
         yearsLegend.push((2024 - i).toString())
     }
+
+
+    console.log("seriesNames", seriesNames)
 
     const option = {
         title: {
@@ -26,16 +28,32 @@ const LineChartEmpPubPri = ({ numSeries, seriesData, seriesNames }) => {
         xAxis: {
             type: 'category',
             data: yearsLegend.reverse(),
-            name: 'Año',
         },
-        yAxis: {
-            type: 'value',
-            name: 'Valor',
-        },
+        yAxis: [
+            {
+                type: 'value',
+                name: seriesNames[1],
+                position: 'left',
+                max: 20000,
+                axisLabel: {
+                    color: '#79bd1aff',
+                },
+            },
+            {
+                type: 'value',
+                name: seriesNames[0],
+                position: 'right',
+                max: 10000,
+                axisLabel: {
+                    color: '#1a6ebdff',
+                },
+            },
+        ],
         series: seriesData.map((data, index) => ({
             name: seriesNames[index],
             type: 'line',
             data: [...data].reverse(),
+            yAxisIndex: index === 1 ? 1 : 0, // La segunda serie usa el eje derecho
         })),
     }
 
