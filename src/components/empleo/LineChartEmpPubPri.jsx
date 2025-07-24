ca26f7bc03a7d81a7eaba2daa997ae58f9a0eff9
rub-ca <ruben.cardin.d@gmail.com>
2025-07-24 18:26:47 +0200

    wip

--- src/components/empleo/LineChartEmpPubPri.jsx
+++ src/components/empleo/LineChartEmpPubPri.jsx
@@ -3,15 +3,20 @@ import React, { lazy, Suspense } from "react"
 const ReactECharts = lazy(() => import('echarts-for-react'))
 
 const LineChartEmpPubPri = ({ numSeries, seriesData, seriesNames }) => {
+    console.log("\n\n")
     const numYears = seriesData[0].length
 
-    const yearsLegend = []
+    let yearsLegend = []
     for (let i = 0; i < numYears; i++) {
         yearsLegend.push((2024 - i).toString())
     }
 
+    const reversedYearsLegend = yearsLegend.reverse()
+    const reversedSeriesData = seriesData.map(data => [...data].reverse())
 
     console.log("seriesNames", seriesNames)
+    console.log("reversedYearsLegend", reversedYearsLegend)
+    console.log("reversedSeriesData", reversedSeriesData)
 
     const option = {
         title: {
@@ -27,33 +32,32 @@ const LineChartEmpPubPri = ({ numSeries, seriesData, seriesNames }) => {
         },
         xAxis: {
             type: 'category',
-            data: yearsLegend.reverse(),
+            data: reversedYearsLegend,
         },
         yAxis: [
             {
                 type: 'value',
-                name: seriesNames[1],
+                name: seriesNames[0],
                 position: 'left',
-                max: 20000,
+                axisLabel: {
+                    color: '#1a6ebdff',
+                },
+                max: 10000
+            },
+            {
+                type: 'value',
+                name: seriesNames[1],
+                position: 'right',
                 axisLabel: {
                     color: '#79bd1aff',
                 },
             },
-            {
-                type: 'value',
-                name: seriesNames[0],
-                position: 'right',
-                max: 10000,
-                axisLabel: {
-                    color: '#1a6ebdff',
-                },
-            },
         ],
-        series: seriesData.map((data, index) => ({
+        series: reversedSeriesData.map((data, index) => ({
             name: seriesNames[index],
             type: 'line',
-            data: [...data].reverse(),
-            yAxisIndex: index === 1 ? 1 : 0, // La segunda serie usa el eje derecho
+            data: data,
+            yAxisIndex: index === 1 ? 1 : 0,
         })),
     }
 
