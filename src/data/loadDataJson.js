742825a885826eac058a5e177eb13024b4340d35
rub-ca <ruben.cardin.d@gmail.com>
2025-08-02 19:27:19 +0200

    wip

--- src/data/loadDataJson.js
+++ src/data/loadDataJson.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb'
+import JSZip from 'jszip'
 
 // Abrir o crear la base de datos
 const dbPromise = openDB('espanaencifras-es-db', 2, {
@@ -8,12 +9,13 @@ const dbPromise = openDB('espanaencifras-es-db', 2, {
 })
 
 export async function loadDataJson(path, setter, setLoading) {
+    const BD_PATH = 'json-files-2024'
     if (setLoading) setLoading(true)
 
     const db = await dbPromise
 
     // Intentar obtener datos de IndexedDB
-    let json = await db.get('json-files-2024', path)
+    let json = await db.get(BD_PATH, path)
 
     if (!json) {
         // Si no está en DB, hacer fetch
@@ -25,7 +27,7 @@ export async function loadDataJson(path, setter, setLoading) {
         json = await res.json()
 
         // Guardar en IndexedDB
-        await db.put('json-files-2024', json, path)
+        await db.put(BD_PATH, json, path)
     }
 
     // Setear los datos en React
@@ -35,29 +37,70 @@ export async function loadDataJson(path, setter, setLoading) {
 }
 
 
-    // const loadZipFromPublic = async () => {
-    //     try {
-    //         console.log('Cargando ZIP desde public/')
-    //         const response = await fetch('/your.zip') // archivo dentro de public/
-    //         if (!response.ok) throw new Error('Error al cargar el archivo ZIP')
+export async function loadDataZipJson(path, setter, setLoading) {
+    const BD_PATH = 'zip-jsn-files-2024'
+    if (setLoading) setLoading(true)
 
-    //         console.log('Archivo ZIP cargado correctamente')
-    //         const blob = await response.blob()
+    const db = await dbPromise
 
-    //         const zip = await JSZip.loadAsync(blob)
-    //         console.log('ZIP cargado:', zip)
+    // Intentar obtener datos de IndexedDB
+    let json = await db.get(BD_PATH, path)
+
+    if (!json) {
+        // Si no está en DB, hacer fetch
+        const res = await fetch("https://data.xn--espaaencifras-lkb.es" + path)
+        if (!res.ok) {
+            if (setLoading) setLoading(false)
+            throw new Error("Error al cargar los datos")
+        }
+        const blob = await res.blob()
+        const zip = await JSZip.loadAsync(blob)
+
+        const [filename] = Object.keys(zip.files)
+        const file = zip.files[filename]
+
+        if (!filename.endsWith('.json')) {
+            throw new Error(`El archivo dentro del ZIP no es un JSON: ${filename}`)
+        }
+
+        const fileContent = await file.async('string')
+        // const jsonData = JSON.parse(fileContent)
+        json = JSON.parse(fileContent)
+        // Guardar en IndexedDB
+        await db.put(BD_PATH, json, path)
+    }
+
+    // Setear los datos en React
+    await setter(json)
+
+    if (setLoading) setLoading(false)
+}
 
 
-    //         for (const filename of Object.keys(zip.files)) {
-    //             if (filename.endsWith('.json')) {
-    //                 const fileContent = await zip.files[filename].async('string')
-    //                 const jsonData = JSON.parse(fileContent)
-    //                 console.log(`Contenido de ${filename}:`, jsonData)
-    //             }
-    //         }
-    //     } catch (error) {
-    //         console.error('Error al leer el ZIP:', error)
-    //     }
-    // }
 
-    // loadZipFromPublic()
+// const loadZipFromPublic = async () => {
+//     try {
+//         console.log('Cargando ZIP desde public/')
+//         const response = await fetch('/your.zip') // archivo dentro de public/
+//         if (!response.ok) throw new Error('Error al cargar el archivo ZIP')
+
+//         console.log('Archivo ZIP cargado correctamente')
+//         const blob = await response.blob()
+
+//         const zip = await JSZip.loadAsync(blob)
+//         console.log('ZIP cargado:', zip)
+
+
+//         for (const filename of Object.keys(zip.files)) {
+//             if (filename.endsWith('.json')) {
+//                 const fileContent = await zip.files[filename].async('string')
+//                 const jsonData = JSON.parse(fileContent)
+//                 console.log(`Contenido de ${filename}:`, jsonData)
+//             }
+//         }
+//     } catch (error) {
+//         console.error('Error al leer el ZIP:', error)
+//     }
+// }
+
+// loadZipFromPublic()
