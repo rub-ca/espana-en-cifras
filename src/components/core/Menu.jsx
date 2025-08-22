d7dcc82bb267fa75ec56c72babc844f82c018dcb
rub-ca <ruben.cardin.d@gmail.com>
2025-08-22 09:37:01 +0200

    wip

--- src/components/core/Menu.jsx
+++ src/components/core/Menu.jsx
@@ -41,6 +41,7 @@ const Menu = ({ showMenu, setShowMenu }) => {
         )
     }
 
+    
     const itemMotion = {
         hidden: { opacity: 0, y: -20 },
         visible: { opacity: 1, y: 0, transition: { duration: 0.3, ease: "easeOut" } }
