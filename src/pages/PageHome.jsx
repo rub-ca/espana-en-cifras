ebd6d1985847471129ee10b7f02aec5db502515c
rub-ca <ruben.cardin.d@gmail.com>
2025-09-01 17:32:59 +0200

    wip

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -6,36 +6,42 @@ const PageHome = () => {
         {
             title: "Población por provincias",
             classItem: "home-item-title-poblacion",
+            classContainer: "home-item-container-poblacion",
             image: "prov-pais.webp",
             content: <h4>Datos de población España, segmentación por provincias.</h4>
         },
         {
             title: "Screener de municipios",
             classItem: "home-item-title-poblacion",
+            classContainer: "home-item-container-poblacion",
             image: "muni-screener.webp",
             content: <h4>Screener que permite buscar municipios filtrando por ciertos parámetros.</h4>
         },
         {
             title: "Población por municipio y edad",
             classItem: "home-item-title-poblacion",
+            classContainer: "home-item-container-poblacion",
             image: "muni-edad.webp",
             content: <h4>Información demográfica por edad a nivel municipal en España.</h4>
         },
         {
             title: "Población por municipio y pais",
             classItem: "home-item-title-poblacion",
+            classContainer: "home-item-container-poblacion",
             image: "muni-pais.webp",
             content: <h4>Distribución de la población por país de origen en los municipios españoles.</h4>
         },
         {
             title: "Empleo público y privado",
             classItem: "home-item-title-empleo",
+            classContainer: "home-item-container-empleo",
             image: "empleo-pubpri.webp",
             content: <h4>Distribución de la población por tipo de empleo (público / privado).</h4>
         },
         {
             title: "Balance pagos y trabajo",
             classItem: "home-item-title-empleo",
+            classContainer: "home-item-container-empleo",
             image: "balance-pagos.webp",
             content: <h4>Tabla interactiva que muestra el balance de pagos y trabajo de España.</h4>
         }
@@ -55,14 +61,14 @@ const PageHome = () => {
 export default PageHome
 
 
-const HomePageItem = ({ title, classItem, image, content }) => {
+const HomePageItem = ({ title, classItem, classContainer, image, content }) => {
     return (
-        <div className="home-item-container">
+        <div className={`home-item-container ${classContainer}`}>
             <div className="home-item-data">
                 <h1 className={`home-item-data-title ${classItem}`}>{title}</h1>
 
                 <div className="home-item-data-content">{content}</div>
-                
+
                 <div className="home-item-data-image">
                     <img src={image} alt="" />
                 </div>
