cd7d387607f99fff86736c1f0f706c6c67df9766
rub-ca <ruben.cardin.d@gmail.com>
2025-09-01 16:58:21 +0200

    wip

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -2,16 +2,51 @@ import React, { useState, useEffect } from "react"
 
 const PageHome = () => {
 
+    const arrayInfo = [
+        {
+            title: "Población por provincias",
+            classItem: "home-item-title-poblacion",
+            image: "prov-pais.webp",
+            content: <h4>Datos de población España, segmentación por provincias.</h4>
+        },
+        {
+            title: "Screener de municipios",
+            classItem: "home-item-title-poblacion",
+            image: "muni-screener.webp",
+            content: <h4>Screener que permite buscar municipios filtrando por ciertos parámetros.</h4>
+        },
+        {
+            title: "Población por municipio y edad",
+            classItem: "home-item-title-poblacion",
+            image: "muni-edad.webp",
+            content: <h4>Información demográfica por edad a nivel municipal en España.</h4>
+        },
+        {
+            title: "Población por municipio y pais",
+            classItem: "home-item-title-poblacion",
+            image: "muni-pais.webp",
+            content: <h4>Distribución de la población por país de origen en los municipios españoles.</h4>
+        },
+        {
+            title: "Empleo público y privado",
+            classItem: "home-item-title-empleo",
+            image: "empleo-pubpri.webp",
+            content: <h4>Distribución de la población por tipo de empleo (público / privado).</h4>
+        },
+        {
+            title: "Balance pagos y trabajo",
+            classItem: "home-item-title-empleo",
+            image: "balance-pagos.webp",
+            content: <h4>Tabla interactiva que muestra el balance de pagos y trabajo de España.</h4>
+        }
+    ]
+
     return (
         <div className="page-data-container">
             <div className="page-data-container__100 overflow-auto flex-grap">
-
-                <HomePageItem></HomePageItem>
-                <HomePageItem></HomePageItem>
-                <HomePageItem></HomePageItem>
-                <HomePageItem></HomePageItem>
-
-
+                {arrayInfo.map((item, index) => (
+                    <HomePageItem key={index} {...item} />
+                ))}
             </div>
         </div>
     )
@@ -20,13 +55,13 @@ const PageHome = () => {
 export default PageHome
 
 
-const HomePageItem = () => {
+const HomePageItem = ({ title, classItem, image, content }) => {
     return (
         <div className="home-item-container">
             <div className="home-item-data">
-                <h1 className="home-item-title">Hello world!</h1>
-                <h4>Datos población España, segmentados por provincias.</h4>
-                <img src="prov-pais.webp" alt="" />
+                <h1 className={`home-item-title ${classItem}`}>{title}</h1>
+                {content}
+                <img src={image} alt="" />
             </div>
         </div>
     )
